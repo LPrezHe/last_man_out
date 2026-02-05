1bd0f7887b6954d4dd7c7c312f8fb496534524ec
LPrezHe <luis.elipeperezherrera@gmail.com>
2026-02-05 17:52:37 -0500

    fix auth

--- auth.py
+++ auth.py
@@ -1,7 +1,13 @@
-from passlib.hash import bcrypt
+import bcrypt
 
-def hash_password(pw):
-    return bcrypt.hash(pw)
+def hash_password(password: str) -> str:
+    pw = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pw, salt)
+    return hashed.decode("utf-8")
 
-def verify_password(pw, hashed):
-    return bcrypt.verify(pw, hashed)
+def verify_password(password: str, hashed: str) -> bool:
+    pw = password.encode("utf-8")
+    hashed = hashed.encode("utf-8")
+    return bcrypt.checkpw(pw, hashed)
+
